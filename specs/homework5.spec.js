eee64062737cb7adbc01ad9dcc3030e31bb51cbd
Marina <marinich135@gmail.com>
2022-04-02 14:27:55 +0300

    Homeworks 3, 4, 5

---
+++ specs/homework5.spec.js
@@ -0,0 +1,81 @@
+import fetch from 'node-fetch';
+
+const URL = 'https://airportgap.dev-tester.com/';
+describe('Отправляем http запросы', () => {
+  test('Получить список аэропортов get api/airports 200', async () => {
+    const path = 'api/airports';
+    const response = await fetch(URL + path, { method: 'GET' });
+    expect(response.status)
+      .toEqual(200);
+  });
+
+  test('Получить описание аэропорта по его id get api/airports/:id 200', async () => {
+    const path = 'api/airports/KIX';
+    const response = await fetch(URL + path, { method: 'GET' });
+    const info = await response.json();
+    console.log(info);
+    expect(info.data.attributes.city).toEqual('Osaka');
+    // expect(response.status).toEqual(200);
+  });
+
+  test('Получить расстояние между двумя заданными аэропортами post  /airports/distance 200', async () => {
+    const path = 'api/airports/distance';
+    const iatacode = {
+      from: 'NRT',
+      to: 'KIX',
+    };
+    const response = await fetch(URL + path, { method: 'POST', body: JSON.stringify(iatacode) });
+    /* console.log(response);
+    const data = await response.json();
+    console.log(data); */
+    expect(response.status)
+      .toEqual(200);
+  });
+
+  test('Получить токен для аккаунта post  /tokens 200', async () => {
+    const path = 'api/tokens';
+    const credentials = {
+      email: 'test@airportgap.com',
+      password: 'airportgappassword',
+    };
+  const response = await fetch(URL + path, { method: 'POST', body: JSON.stringify(credentials) });
+    /*console.log(response);
+    const data = await response.json();
+    console.log(data);*/
+    expect(response.status).toEqual(200);
+  });
+
+  test('Сохранить любимый аэропорт post  /favorites', async () => {
+    const path = 'api/favorites?';
+    const params = new URLSearchParams(
+        {
+          airport_id: "JFK"
+        },
+    );
+    const response = await fetch(URL + path + params, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        Authorization:'Bearer token=9WHkBEdbYYYzDSoMkV9UjZjh'
+      }
+    });
+    expect(response.status).toEqual(201);
+
+  });
+
+  test('Получить любимые аэропорты get  /favorites', async () => {
+    const path = 'api/favorites';
+
+    const response = await fetch(URL + path , {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization:'Bearer token=9WHkBEdbYYYzDSoMkV9UjZjh'
+      }
+    });
+    expect(response.status).toEqual(200);
+
+  });
+
+
+});
