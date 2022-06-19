7b2a5e1af1ee41e9ca4fc182b31700991fefdb96
Marina <marinich135@gmail.com>
2022-06-19 15:21:58 +0300

    Homework 10. Clone of Homework9.

--- framework/config/urls.js
+++ framework/config/urls.js
@@ -1,5 +1,4 @@
 const urls = {
-  airport: 'https://airportgap.dev-tester.com/',
   cats: 'https://api.thecatapi.com/',
 };
 export default urls;
--- framework/services/index.js
+++ framework/services/index.js
@@ -1,13 +1,7 @@
-import Airports from './airports.service';
-import Tokens from './tokens.service';
-import Favorites from './favorites.service';
 import Breeds from './breeds.service';
 import Categories from './categories.service';
 
 const api = () => ({
-  Airports: () => ({ ...Airports }),
-  Tokens: () => ({ ...Tokens }),
-  Favorites: () => ({ ...Favorites }),
   Breeds: () => ({ ...Breeds }),
   Categories: () => ({ ...Categories }),
 
