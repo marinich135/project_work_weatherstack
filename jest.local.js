c7202ae4331f1760322f8f0f35f49f1c15fb23b8
Marina <marinich135@gmail.com>
2022-06-26 16:51:42 +0300

    add report.html

--- jest.local.js
+++ jest.local.js
@@ -12,7 +12,10 @@ module.exports = {
   },
   reporters: [
     'default',
-    'jest-html-reporters',
+    'jest-html-reporters', {
+      filename: 'report.html',
+      openReport: true,
+    },
   ],
   verbose: true,
 };
